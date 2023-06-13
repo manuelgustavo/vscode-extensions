2f05f319e91e3d9946a896deed1c054fa77e9ad3
Manuel Saraiva <manuel.gustavo@gmail.com>
2023-06-13 14:31:25 +0100

    all at a single call once

--- vscode-extensions.sh
+++ vscode-extensions.sh
@@ -1,46 +1,47 @@
 #!/bin/sh
-code --install-extension aaron-bond.better-comments
-code --install-extension alefragnani.Bookmarks
-code --install-extension baincd.copy-path-unixstyle
-code --install-extension cheshirekow.cmake-format
-code --install-extension cschlosser.doxdocgen
-code --install-extension DavidAnson.vscode-markdownlint
-code --install-extension deckerio.breakpointio
-code --install-extension donjayamanne.githistory
-code --install-extension DotJoshJohnson.xml
-code --install-extension dtsvet.vscode-wasm
-code --install-extension fernandoescolar.vscode-solution-explorer
-code --install-extension foxundermoon.shell-format
-code --install-extension fredericbonnet.cmake-test-adapter
-code --install-extension GitLab.gitlab-workflow
-code --install-extension hbenl.vscode-test-explorer
-code --install-extension jeff-hykin.better-cpp-syntax
-code --install-extension josetr.cmake-language-support-vscode
-code --install-extension maattdd.gitless
-code --install-extension matepek.testmate-cpp-legacy
-code --install-extension mhutchie.git-graph
-code --install-extension ms-azuretools.vscode-docker
-code --install-extension ms-dotnettools.vscode-dotnet-runtime
-code --install-extension ms-vscode-remote.remote-containers
-code --install-extension ms-vscode-remote.remote-ssh
-code --install-extension ms-vscode-remote.remote-ssh-edit
-code --install-extension ms-vscode-remote.remote-wsl
-code --install-extension ms-vscode-remote.vscode-remote-extensionpack
-code --install-extension ms-vscode.cmake-tools
-code --install-extension ms-vscode.cpptools
-code --install-extension ms-vscode.cpptools-extension-pack
-code --install-extension ms-vscode.cpptools-themes
-code --install-extension ms-vscode.powershell
-code --install-extension ms-vscode.test-adapter-converter
-code --install-extension mtib.vsgx
-code --install-extension natessilva.ts-prop-dag
-code --install-extension oderwat.indent-rainbow
-code --install-extension Remisa.shellman
-code --install-extension rogalmic.bash-debug
-code --install-extension shaharkazaz.git-merger
-code --install-extension technosophos.vscode-make
-code --install-extension timonwong.shellcheck
-code --install-extension twxs.cmake
-code --install-extension usernamehw.errorlens
-code --install-extension xaver.clang-format
-code --install-extension yzhang.markdown-all-in-one
+code --install-extension aaron-bond.better-comments \
+     --install-extension alefragnani.Bookmarks \
+     --install-extension baincd.copy-path-unixstyle \
+     --install-extension cheshirekow.cmake-format \
+     --install-extension cschlosser.doxdocgen \
+     --install-extension DavidAnson.vscode-markdownlint \
+     --install-extension deckerio.breakpointio \
+     --install-extension donjayamanne.githistory \
+     --install-extension DotJoshJohnson.xml \
+     --install-extension dtsvet.vscode-wasm \
+     --install-extension fernandoescolar.vscode-solution-explorer \
+     --install-extension foxundermoon.shell-format \
+     --install-extension fredericbonnet.cmake-test-adapter \
+     --install-extension GitLab.gitlab-workflow \
+     --install-extension hbenl.vscode-test-explorer \
+     --install-extension jeff-hykin.better-cpp-syntax \
+     --install-extension josetr.cmake-language-support-vscode \
+     --install-extension maattdd.gitless \
+     --install-extension matepek.testmate-cpp-legacy \
+     --install-extension mhutchie.git-graph \
+     --install-extension ms-azuretools.vscode-docker \
+     --install-extension ms-dotnettools.vscode-dotnet-runtime \
+     --install-extension ms-vscode-remote.remote-containers \
+     --install-extension ms-vscode-remote.remote-ssh \
+     --install-extension ms-vscode-remote.remote-ssh-edit \
+     --install-extension ms-vscode-remote.remote-wsl \
+     --install-extension ms-vscode-remote.vscode-remote-extensionpack \
+     --install-extension ms-vscode.cmake-tools \
+     --install-extension ms-vscode.cpptools \
+     --install-extension ms-vscode.cpptools-extension-pack \
+     --install-extension ms-vscode.cpptools-themes \
+     --install-extension ms-vscode.powershell \
+     --install-extension ms-vscode.test-adapter-converter \
+     --install-extension mtib.vsgx \
+     --install-extension natessilva.ts-prop-dag \
+     --install-extension oderwat.indent-rainbow \
+     --install-extension Remisa.shellman \
+     --install-extension rogalmic.bash-debug \
+     --install-extension shaharkazaz.git-merger \
+     --install-extension technosophos.vscode-make \
+     --install-extension timonwong.shellcheck \
+     --install-extension twxs.cmake \
+     --install-extension usernamehw.errorlens \
+     --install-extension xaver.clang-format \
+     --install-extension yzhang.markdown-all-in-one
+ 
