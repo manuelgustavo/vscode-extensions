26c0ecbbee10ee36ffdcfebb1ea2b9a56562b0c1
Manuel Saraiva <manuel.gustavo@gmail.com>
2025-04-23 12:05:17 +0100

    Update 2025/04

--- vscode-extensions.sh
+++ vscode-extensions.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
 code --install-extension aaron-bond.better-comments
 code --install-extension alefragnani.bookmarks
+code --install-extension alexd.perfanno
 code --install-extension augustocdias.tasks-shell-input
 code --install-extension baincd.copy-path-unixstyle
 code --install-extension cheshirekow.cmake-format
 code --install-extension cschlosser.doxdocgen
 code --install-extension davidanson.vscode-markdownlint
 code --install-extension deckerio.breakpointio
+code --install-extension docker.docker
 code --install-extension donjayamanne.githistory
 code --install-extension dotjoshjohnson.xml
-code --install-extension dtsvet.vscode-wasm
 code --install-extension fernandoescolar.vscode-solution-explorer
 code --install-extension foxundermoon.shell-format
-code --install-extension fredericbonnet.cmake-test-adapter
-code --install-extension hbenl.vscode-test-explorer
 code --install-extension hediet.vscode-drawio
 code --install-extension jeff-hykin.better-cpp-syntax
 code --install-extension jock.svg
@@ -21,6 +20,7 @@ code --install-extension josetr.cmake-language-support-vscode
 code --install-extension maattdd.gitless
 code --install-extension matepek.vscode-catch2-test-adapter
 code --install-extension mhutchie.git-graph
+code --install-extension mongodb.mongodb-vscode
 code --install-extension ms-azuretools.vscode-docker
 code --install-extension ms-dotnettools.vscode-dotnet-runtime
 code --install-extension ms-python.debugpy
@@ -40,16 +40,12 @@ code --install-extension ms-vscode.remote-explorer
 code --install-extension ms-vscode.remote-server
 code --install-extension ms-vscode.test-adapter-converter
 code --install-extension mtib.vsgx
-code --install-extension natessilva.ts-prop-dag
 code --install-extension oderwat.indent-rainbow
 code --install-extension remisa.shellman
-code --install-extension rogalmic.bash-debug
 code --install-extension shaharkazaz.git-merger
 code --install-extension stuart.unique-window-colors
 code --install-extension technosophos.vscode-make
-code --install-extension timonwong.shellcheck
 code --install-extension twxs.cmake
-code --install-extension uctakeoff.vscode-counter
 code --install-extension usernamehw.errorlens
 code --install-extension xaver.clang-format
 code --install-extension yzhang.markdown-all-in-one
